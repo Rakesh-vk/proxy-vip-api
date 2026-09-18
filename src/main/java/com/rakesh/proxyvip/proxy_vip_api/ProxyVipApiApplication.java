6ae5754e1a5c939b823d4551cd6b1dc87ae5c284
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-09-18 11:41:19 +0530

    creating project

---
+++ src/main/java/com/rakesh/proxyvip/proxy_vip_api/ProxyVipApiApplication.java
@@ -0,0 +1,13 @@
+package com.rakesh.proxyvip.proxy_vip_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyVipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyVipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakesh/proxyvip/proxy_vip_api/ProxyVipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.proxyvip.proxy_vip_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyVipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
