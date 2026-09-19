10fbfb0dc47614980c1ec0e1d772bece3ab67868
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-09-20 01:31:39 +0530

    adding pattern based validation for input IPs

--- src/main/java/com/rakesh/proxyvip/proxy_vip_api/dto/AddVipRequest.java
+++ src/main/java/com/rakesh/proxyvip/proxy_vip_api/dto/AddVipRequest.java
@@ -1,8 +1,13 @@
 package com.rakesh.proxyvip.proxy_vip_api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record AddVipRequest(
+        @Pattern(
+                regexp = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+                message = "must be a valid IPv4 address"
+        )
         @NotBlank
         String newVip
 ) {
